246550c218a131b61dad4fc8eaa816047c2189a9
Dominik <marczukd@gmail.com>
2024-08-15 00:12:00 +0200

    add GitHubService

---
+++ src/main/java/com/example/githubmanager/infrastructure/GitHubService.java
@@ -0,0 +1,23 @@
+package com.example.githubmanager.infrastructure;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GitHubService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private static final String GITHUB_API_URl = "https://api.github.com/users/{username}/repos";
+
+    public List<Object> getRepositoriesForUser(String username) {
+        String uri = UriComponentsBuilder.fromUriString(GITHUB_API_URl)
+                .buildAndExpand(username)
+                .toString();
+        Object[] repositories = restTemplate.getForObject(uri, Object[].class);
+
+        return Arrays.asList(repositories);
+    }
+}
