dfc0be59f016cfc9a5cf766c8b260945140e1eb0
Dominik <marczukd@gmail.com>
2024-08-14 21:21:31 +0200

    first commit

---
+++ src/test/java/com/example/githubmanager/GitHubManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
