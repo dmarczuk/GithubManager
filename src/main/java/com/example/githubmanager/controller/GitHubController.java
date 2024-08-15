768398addba101ee03435ed695093ec8c6bae71c
Dominik <marczukd@gmail.com>
2024-08-15 20:58:44 +0200

    add header to method in controller

--- src/main/java/com/example/githubmanager/controller/GitHubController.java
+++ src/main/java/com/example/githubmanager/controller/GitHubController.java
@@ -1,11 +1,13 @@
 package com.example.githubmanager.controller;
 
 
+import com.example.githubmanager.model.GithubRepository;
 import com.example.githubmanager.service.GitHubService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,9 +18,9 @@ public class GitHubController {
 
     private GitHubService gitHubService;
 
-    @GetMapping("/repos/{username}")
-    public ResponseEntity<List<Object>> getRepositories(@PathVariable String username) {
-        List<Object> repositories = gitHubService.createListOfRepositories(username);
+    @GetMapping(value = "/repos/{username}", produces = "application/json")
+    public ResponseEntity<List<GithubRepository>> getRepositories(@RequestHeader(value = "Accept", required = false) @PathVariable String username) {
+        List<GithubRepository> repositories = gitHubService.createListOfRepositories(username);
         return ResponseEntity.ok(repositories);
     }
 }
