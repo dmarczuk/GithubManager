825133769f283d3f63d5a87a8ef75f40c539500f
Dominik <marczukd@gmail.com>
2024-08-15 19:30:00 +0200

    add method getting list of branches for repo

--- src/main/java/com/example/githubmanager/infrastructure/GitHubService.java
+++ src/main/java/com/example/githubmanager/infrastructure/GitHubService.java
@@ -12,14 +12,24 @@ public class GitHubService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    private static final String GITHUB_API_URl = "https://api.github.com/users/{username}/repos";
+    private static final String GITHUB_API_REPOS_URl = "https://api.github.com/users/{username}/repos";
+    private static final String GITHUB_API_BRANCHES_URL = "https://api.github.com/repos/{owner}/{repo}/branches";
 
     public List<Object> getRepositoriesForUser(String username) {
-        String uri = UriComponentsBuilder.fromUriString(GITHUB_API_URl)
+        String uri = UriComponentsBuilder.fromUriString(GITHUB_API_REPOS_URl)
                 .buildAndExpand(username)
                 .toString();
         Object[] repositories = restTemplate.getForObject(uri, Object[].class);
 
         return Arrays.asList(repositories);
+    }
+
+    public List<Object> getBranchesForRepo(String owner, String repo) {
+        String uri = UriComponentsBuilder.fromUriString(GITHUB_API_BRANCHES_URL)
+                .buildAndExpand(owner, repo)
+                .toString();
+        Object[] branches = restTemplate.getForObject(uri, Object[].class);
+
+        return Arrays.asList(branches);
     }
 }
