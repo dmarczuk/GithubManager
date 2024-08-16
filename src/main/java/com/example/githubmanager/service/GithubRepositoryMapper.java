15cc465e04302c91e3d209577bb4ac4e136db11d
Dominik <marczukd@gmail.com>
2024-08-16 09:41:16 +0200

    change GithubRepositoryMapper

--- src/main/java/com/example/githubmanager/service/GithubRepositoryMapper.java
+++ src/main/java/com/example/githubmanager/service/GithubRepositoryMapper.java
@@ -1,22 +1,35 @@
 package com.example.githubmanager.service;
 
 import com.example.githubmanager.model.GithubRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Builder
 public class GithubRepositoryMapper {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static GithubRepository mapToGitHubRepository(Object object) {
-        Map<String, Object> rawMap = (Map<String, Object>)object;
+    public static List<GithubRepository> githubRepositoriesMapperFromJson(String json) {
+        List<GithubRepository> githubRepositories = new ArrayList<>();
+        try {
+            JsonNode rootNode = objectMapper.readTree(json);
 
-        return new GithubRepository(
-                (String)rawMap.get("name"),
-                (String)rawMap.get("full_name"),
-                (String)rawMap.get("branches_url"),
-                (boolean)rawMap.get("fork")
-        );
+            for(JsonNode repoNode: rootNode) {
+                String name = repoNode.path("name").asText();
+                String owner = repoNode.path("owner").path("login").asText();
+                String url_branches = repoNode.path("branches_url").asText();
+                boolean isFork = repoNode.path("fork").asBoolean();
+                GithubRepository githubRepository = new GithubRepository(name, owner, url_branches, isFork);
+                githubRepositories.add(githubRepository);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // message can read json
+        }
+        return githubRepositories;
     }
 }
